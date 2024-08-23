9a51ca9a7ecb2c10aa89cb00c89fcdf0c98c35dd
Cem Baykam <cem@kekik.com.tr>
2024-08-23 16:34:57 +0300

    Add the model and controller tests.

---
+++ test/models/scope_hound/concerns/filterable_model_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "app/models/scope_hound/concerns/filterable_model"
+
+class FilterableModelTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip "Not implemented"
+  end
+end
