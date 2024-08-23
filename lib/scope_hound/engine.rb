8d5c76aaf5f73817900e8d0c5ebbc49f8c7499e6
Cem Baykam <cem@kekik.com.tr>
2024-08-23 23:28:14 +0300

    No need to have an include path anymore.

--- lib/scope_hound/engine.rb
+++ lib/scope_hound/engine.rb
@@ -4,8 +4,5 @@ module ScopeHound
   # Initialize the rails engine for the gem.
   class Engine < ::Rails::Engine
     isolate_namespace ScopeHound
-
-    config.autoload_paths << root.join('lib', 'scope_hound', 'models', 'concerns')
-    config.autoload_paths << root.join('lib', 'scope_hound', 'controllers', 'concerns')
   end
 end
