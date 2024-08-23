f5438e92b9681617c46c68ecd433564e69c7c2a4
Cem Baykam <cem@kekik.com.tr>
2024-08-23 16:32:31 +0300

    Add the test for filterable controller.

---
+++ test/controllers/scope_hound/concerns/filterable_controller_test.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "minitest/autorun"
+require "app/controllers/scope_hound/concerns/filterable_controller"
+
+class FilterableControllerTest < Minitest::Test
+  include FilterableController
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip "Not implemented"
+  end
+end
