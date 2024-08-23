53b62d3728cdfed49006c6b2b3eb46f0cba984ff
Cem Baykam <cem@kekik.com.tr>
2024-08-23 21:56:58 +0300

    Don't need test inside the test directory.

--- lib/scope_hound/engine.rb
+++ lib/scope_hound/engine.rb
@@ -1,14 +1,11 @@
 # frozen_string_literal: true
 
 module ScopeHound
-  # Include the Rails engine
+  # Initialize the rails engine for the gem. 
   class Engine < ::Rails::Engine
     isolate_namespace ScopeHound
 
-    initializer "scope_hound.autoload_paths" do |app|
-      # Auto-load the concerns following Rails conventions
-      config.autoload_paths << root.join('lib', 'scope_hound', 'models', 'concerns')
-      config.autoload_paths << root.join('lib', 'scope_hound', 'controllers', 'concerns')
-    end
+    config.autoload_paths << root.join('lib', 'scope_hound', 'models', 'concerns')
+    config.autoload_paths << root.join('lib', 'scope_hound', 'controllers', 'concerns')
   end
 end
--- spec/dummy/app/controllers/tests_controller.rb
+++ spec/dummy/app/controllers/tests_controller.rb
@@ -1,9 +1,18 @@
 # frozen_string_literal: true
 
 class TestsController < ApplicationController
+  include ScopeHound::Controllers::Concerns::FilterableController
 
   def test_action
     # You can use your gem's logic here
   end
 
+  private
+
+  def filter_params
+    {
+      test_name: params[:post_name],
+      test_status: params[:test_status]
+    }
+  end
 end
